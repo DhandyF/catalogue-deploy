96d57663ecb3b1e1a59c4482e5ea3f79932bdb27
Moch. Dhandy Friyadi <dhandy.friyadi@gmail.com>
2020-11-28 18:46:12 +0700

    catalogue

---
+++ layouts/catalog.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <nav-bar />
+    <div class="container">
+      <div class="content">
+        <div class="sidebar">
+          <p>Filter</p>
+        </div>
+        <div class="catalog">
+          <Nuxt />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.container {
+  width: 100%;
+  margin: 60px 0 0 0;
+  padding: 0 70px;
+}
+
+.content {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  /* border: 1px solid black; */
+}
+
+.sidebar {
+  width: 240px;
+  /* border: 1px solid black; */
+}
+
+.catalog {
+  width: 100%;
+  margin: 0;
+  padding: 20px;
+  /* border: 1px solid yellow; */
+}
+
+.button--purple {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #5c4050;
+  color: #5c4050;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--purple:hover {
+  color: #fff;
+  background-color: #5c4050;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="catalog-container">
+    <div class="search-bar">
+      <input type="text" placeholder="Search product" name="search">
+    </div>
+    <div class="product">
+      <div v-for="i in 7" :key="i" class="product-card">
+        <img class="product-image" src="~/assets/images/kaos-polos1.jpg" alt="kaos polos">
+        <div class="product-desc">
+          <div class="product-title">
+            Kaos Polos Screamous
+          </div>
+          <div class="product-price">
+            Rp.30.000
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'catalog'
+}
+</script>
+
+<style>
+.catalog-container {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.search-bar {
+  width: 100%;
+  margin: 0 0 30px 0;
+  /* border: 1px solid green; */
+}
+
+.search-bar input {
+  width: 100%;
+  padding: 5px 10px;
+  font-size: 15px;
+  font-family: 'Quicksand', sans-serif;
+  border: 1px solid lightgrey;
+  border-radius: 20px;
+}
+
+.search-bar input:focus{
+  outline: none;
+}
+
+.search-bar i {
+  font-size: 15px;
+  background-image: url('~assets/icons/search.svg');
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 1px solid lightgrey;
+  /* border-radius: 0 5px 5px 0; */
+  background-size: 20px 20px;
+}
+
+.product {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+}
+
+.product-card {
+  width: 180px;
+  height: 300px;
+  margin: 6px;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.2);
+  cursor: pointer;
+  /* border: 1px solid grey; */
+}
+
+.product-card:hover {
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
+}
+
+.product-image {
+  width: 100%;
+  height: 170px;
+  margin: 0;
+  padding: 0;
+  border-radius: 10px 10px 0 0;
+}
+
+.product-desc {
+  margin: 0;
+  padding: 10px;
+}
+
+.product-title {
+  margin: 0 0 10px 0;
+  font-family: 'Quicksand-SemiBold', sans-serif;
+  color: darkslategrey;
+  font-size: 17px;
+}
+
+.product-price {
+  font-family: 'Quicksand-Bold', sans-serif;
+  color: #FA591D;
+  font-size: 18px;
+}
+</style>
